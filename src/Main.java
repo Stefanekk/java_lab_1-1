705e0948a6aa5954d0cffef9c6e291fd4affa19a
Bartek Stefański <92265595+Stefanekk@users.noreply.github.com>
2022-01-21 19:00:11 +0100

    cw5

--- src/Main.java
+++ src/Main.java
@@ -10,13 +10,36 @@ public class Main {
          * w innym wypadku "Niepodzielny przez 3" użyj kontrukcji if else
          * */
 
+        int wiek = 29;
+        if(wiek%3==0) {
+            System.out.println("Podzielny przez 3");
+        } else {
+            System.out.println("Niepodzielny przez 3");
+        }
+
 
         /* zad.2 użyj parametru trójargumentowego aby sprawdzić parzystośc nr. indeksu */
 
+        int nrIndex = 56577;
+        int czyParzysty = (nrIndex % 2 == 0) ? 0 : 1 ;
+        System.out.println("3) Czy parzysty : " + czyParzysty);
 
         /* zad.3 wpisz z klawiatury liczbę zmiennoprzecinkową (użyj klasy Scanner z poprzednich ćwiczeń)
          * i stwórz konstrukcję else if z dowolnymi komunikatami */
 
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Podaj ile masz złotych aby kupić coca cole za 5 zł: ");
+        double kasa = scan.nextDouble();
+
+        if (kasa < 5 )
+        {
+            System.out.println(" Zbyt malo pieniędzy na cole");
+        }else if(kasa == 5) {
+            System.out.println("Masz idealnie na jedna butelke coca coli");
+
+        }else if (kasa > 5) {
+            System.out.println("Możesz kupić więcej niż tylko butelke coca coli");
+        }
 
     }
 }
