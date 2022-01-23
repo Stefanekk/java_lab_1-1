4be699a4846252e5360bf86b98826986b1a11007
Bartek Stefański <92265595+Stefanekk@users.noreply.github.com>
2022-01-23 02:33:30 +0100

    commit 1

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,26 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
+        int zmiennaInt = 7;
+        System.out.println("zmienna int = " + zmiennaInt);
+        /* ćw.2 */
         int zmienna1 = 16;
         String imie1 = "Piotr";
         double wiek = 27.11;
         boolean zmiennaBoolean = true;
 
+        /*   zad.1 zadeklaruj i zainicjalizuj zmienne różnych typów i wyświetl w konsoli (im więcej tym lepiej ;) ) */
         System.out.println(imie1);
         System.out.println(zmienna1);
         System.out.println(wiek);
         System.out.print(zmiennaBoolean);
 
+        /*
+         *    zad.2 Napisz kod który będzie wykonywał operacje: dodawania, odejmowania,
+         *    mnożenia, dziealenia i modulo na zmiennych:
+         *    a) liczbaA i liczbaB, dowolne liczby calkowite (int),
+         *    b) liczbaX i liczbaY, dowolne liczby zmiennoprzecinkowe (double)
+         *  */
 
 
         //cw2
@@ -36,6 +45,7 @@ public class Main {
         System.out.println(liczbaX * liczbaY);
         System.out.println(liczbaY/liczbaX);
         System.out.println(liczbaY%liczbaX);
+
 
     }
 }
